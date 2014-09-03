5f1b021c96bc32ebc4fe8b01b0803a69c2e4abfc
Andres Hernandez <ahernandez@growthaccelerationpartners.com>
2014-09-02 21:36:57 -0600

    Remove console.log

--- bg/weatherService.js
+++ bg/weatherService.js
@@ -6,7 +6,6 @@ var WeatherService = {
   findTemperature: function(query_terms, callback) {
     var apiUrl = "http://api.openweathermap.org/data/2.5/find?units=metric&q=" + query_terms;
     $.ajax({ url: apiUrl }).done(function(response) {
-      console.log(response);
       if (response.cod == "200") {
         var item = response.list[0];
         var place = item.name == "" ? item.sys.country: item.name + ", " + item.sys.country;
